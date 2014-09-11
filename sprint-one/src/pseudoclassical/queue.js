a049c294f280c412cbbe73768c3ebe5124040c0e
Derek Chen <derekchen14@gmail.com>
2014-09-11 08:12:13 -0700

    Completed pseudo-classical patterns for stack and queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,24 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+	this.storage = {};
+	this.totalSize = 0;
+	this.first = 0;
 };
 
+Queue.prototype.enqueue = function(val) {
+	this.storage[this.totalSize+this.first] = val;
+	this.totalSize++;
+};
+Queue.prototype.dequeue = function() {
+	if (this.totalSize > 0) {
+		this.totalSize--;
+		var results = this.storage[this.first];
+		delete this.storage[this.first];
+		this.first++;
+	}
+	return results;
+};
+Queue.prototype.size = function() {
+	return this.totalSize;
+};
 
+// var newQueue = new Queue();
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,22 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+	this.storage = {};
+	this.totalSize = 0;
 };
 
+Stack.prototype.push = function(val) {
+	this.storage[this.totalSize] = val;
+	this.totalSize++;
+};
+Stack.prototype.pop = function() {
+	if (this.totalSize > 0) {
+		this.totalSize--;
+		var results = this.storage[this.totalSize];
+		delete this.storage[this.totalSize];
+	}
+	return results;
+};
+Stack.prototype.size = function() {
+	return this.totalSize;
+};
 
+// var newStack = new Stack();
