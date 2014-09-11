7e04e9b9e91f5a39d01fc2aa5fd383cee2c740a0
Derek Chen <derekchen14@gmail.com>
2014-09-10 19:54:57 -0700

    Completed functional instantiation patterns for stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = first = 0;
 
   // Implement the methods below
-
   someInstance.enqueue = function(value){
+    storage[size + first] = value;
+    size++;
   };
 
   someInstance.dequeue = function(){
+    var result;
+    if (size > 0) {
+      size--;
+      result = storage[first];
+      delete storage.first;
+      first++;
+    }
+    return result;
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
